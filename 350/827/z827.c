05c3c61c1504dddcb3d80baaad3be6168b3b06c3
Amber Dean <amberwesleydean@gmail.com>
2026-02-27 15:30:42 -0500

    got argument interaction working, and can open, create, read and write files

--- 350/827/z827.c
+++ 350/827/z827.c
@@ -1,47 +1,77 @@
 #include <stdio.h>
 #include <string.h>
-int main(int argc, char *argv[]){
+#include <unistd.h>
+#include <fcntl.h>
 
+int main(int argc, char* argv[]){
+    if(argc > 2){
+        fprintf(stderr,"Error: too many arguments\n");
+        return 1;
+    } else if(argc < 2){
+        fprintf(stderr,"Error: too few arguments\n");
+        return 1;
+    }
     //extensions to compare
     char extension[] = ".z827";
     char extension2[] = ".txt";
     //string lengths
-    int strln = strlen(argv);
+    int strln = strlen(argv[1]);
     int exln = strlen(extension);
     int ex2ln = strlen(extension2);
     //file flags
-    int compress = 1;
-    int validfile = 1;
+    int decompress = 0; //flag for if file ends with .z827
+    int validtxt = 0;   //flag for if file ends with .txt
 
-    int fileloc = open(*argv);
+    int fileloc = open(argv[1], O_RDWR);
+    //Throws error if file doesnt exist
+    if(fileloc < 0){
+        fprintf(stderr,"ERROR: Given file does not exist\n");
+        return 1;
+    }
 
     //compare end of given filename to expected extension for decompression and set flag accordingly
     for(int i = 0; i < strln && i < exln; i++){
-        if (argv[i+strln-exln] != extension[i]){
-            compress = 1;
+        if ((char)argv[1][i+strln-exln] != extension[i]){
+            decompress = 0;
             break;
         }
-        compress = 0;
+        decompress = 1;
     }
     //check for hedge case where string is too small to contain extension
     if(strln < exln){
-        compress = 1;
+        decompress = 0;
     }
-
-    if(compress){
-    //checks that given file is even a .txt
-    for(int i = 0; i < strln && i < ex2ln; i++){
-        if (argv[i+strln-ex2ln] != extension2[i]){
-            validfile = 0;
-            break;
+    //checks that given file is a .txt and possibly throws error
+    if(!decompress){
+        for(int i = 0; i < strln && i < ex2ln; i++){
+            if ((char) argv[1][i+strln-ex2ln] != extension2[i]){
+                validtxt = 0;
+                fprintf(stderr,"ERROR: Given file not correct format");
+                return 1;
+            }
+        validtxt = 1;
         }
-        validfile = 1;
     }
-    }else{
+    //check for hedge case where string is too small to contain extension
+    if(strln < ex2ln){
+    fprintf(stderr,"ERROR: Given file not correct format");
+    return 1;
+    }
+    
 
-}   
 
-
+    //COMPRESSION ALGO
+    unsigned int buf = 0;
+    unsigned int filesizebyte = lseek(fileloc, 0 ,SEEK_END);
+    if(!decompress){
+        int newfileloc = creat(strcat(argv[1], ".z827"), O_WRONLY);
+        write(newfileloc, filesizebyte, 1);
+        for(int i = 0; i < filesizebyte; i++){
+            
+        }
+    }
+    //TESTING
+    printf("decompress: %d, validtxt: %d, fileloc: %d, filesizebyte: %d\n", decompress, validtxt, fileloc, filesizebyte);
 
     return 0;
 }
