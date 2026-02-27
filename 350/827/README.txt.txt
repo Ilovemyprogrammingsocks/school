1e815467dad04548d15d8d599feb7700d3a17a2b
unknown <royallymassiveoof@gmail.com>
2026-02-26 20:54:10 -0500

    readme and extension checking Signed-off-by: Amber Dean <amberwelseydean@gmail.com>

---
+++ 350/827/README.txt.txt
@@ -0,0 +1,3 @@
+Amber Dean
+
+C Program to compress ASCII text files by removing the common unused bit and going from "8 to 7" bits per character
--- 350/827/z827.c
+++ 350/827/z827.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char *argv[]){
-    
+
+    //extensions to compare
+    char extension[] = ".z827";
+    char extension2[] = ".txt";
+    //string lengths
+    int strln = strlen(argv);
+    int exln = strlen(extension);
+    int ex2ln = strlen(extension2);
+    //file flags
+    int compress = 1;
+    int validfile = 1;
+
+    int fileloc = open(*argv);
+
+    //compare end of given filename to expected extension for decompression and set flag accordingly
+    for(int i = 0; i < strln && i < exln; i++){
+        if (argv[i+strln-exln] != extension[i]){
+            compress = 1;
+            break;
+        }
+        compress = 0;
+    }
+    //check for hedge case where string is too small to contain extension
+    if(strln < exln){
+        compress = 1;
+    }
+
+    if(compress){
+    //checks that given file is even a .txt
+    for(int i = 0; i < strln && i < ex2ln; i++){
+        if (argv[i+strln-ex2ln] != extension2[i]){
+            validfile = 0;
+            break;
+        }
+        validfile = 1;
+    }
+    }else{
+
+}   
+
+
 
     return 0;
 }
